72b195092cfd3ce4b3729ea31312a44991b22890
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2024-06-09 23:32:41 +0300

    Initial Commit

---
+++ MusicPlayer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MusicPlayer
+//
+//  Created by Даниил Суханов on 09.06.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
